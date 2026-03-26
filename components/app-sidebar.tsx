d3799772e7d704f71491ffc21b970a9a461f19a0
ceosiva <ceosivaofficial@gmail.com>
2026-03-26 13:35:43 +0530

    mongodb configured

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -186,9 +186,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:p-1.5!"
             >
-              <a href="#">
-                <CommandIcon className="size-5!" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+              <a href="/">
+                <img src="/assets/farm-greens-logo.png" alt="FarmGreens Logo" className="size-10 object-contain" />
+                <span className="text-base font-semibold">FarmGreens</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
